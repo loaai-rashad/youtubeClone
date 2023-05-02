64ed3bce333b4fa2eeb82bcf97ed386f987dada7
loaai-rashad <rashadloaai@gmail.com>
2023-05-02 13:52:03 +0200

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import { Stack } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+import { logo } from '../utils/constants';
+import SearchBar from './SearchBar';
+
+
+const Navbar = () => (
+  <Stack 
+  direction="row" 
+  alignItem="center" 
+  p={2} 
+  sx={{ position: 'sticky', background: '#000' , top:0, justifycontent: 'space-between'}}
+  >
+    <Link to="/" style={{ display: 'flex', alignitem: 'center' }}>
+    <img src= {logo} alt="logo" height={45} />
+    </Link>
+    <SearchBar />
+  </Stack>
+)
+
+
+export default Navbar
